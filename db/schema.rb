87379a88a79701fce530cc246c0c23959e31a7a3
Art <art19ad4@gmail.com>
2015-06-19 12:26:08 -0700

    added devise to make admin user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619171646) do
+ActiveRecord::Schema.define(version: 20150619191220) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "full_name"
@@ -23,12 +23,6 @@ ActiveRecord::Schema.define(version: 20150619171646) do
   end
 
   create_table "departments", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
